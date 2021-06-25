886251e0aca7c8aedaadc6bd6b5514ec165d562e
MohammedRaslan <mohamedraslan2282@yahoo.com>
2021-06-25 21:20:46 +0200

    Initial commit

---
+++ tests/Feature/BookReservationTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BookReservationTest extends TestCase
+{
+    use RefreshDatabase;
+   /** @test */
+   public function a_book_can_be_added_to_the_library()
+   {
+       $this->withoutExceptionHandling();
+       $response = $this->post('/books',[
+           'title' => 'Cool Book',
+           'author' => 'Mohamed Raslan'
+       ]);
+       $response->assertOk();
+       $this->assertCount(1, Book::all());
+   }
+}
